22fd28f4737bc6f60a9b1c897fbcb111d848441a
Ashton Heald <devtheash@gmail.com>
2025-04-03 14:14:08 -0500

    building base web layout

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+
+const navItems = [
+  { text: "Home", href: "/" },
+  { text: "About", href: "#about" },
+  { text: "Skills", href: "#skills" },
+  { text: "Projects", href: "#projects" },
+  { text: "Contact", href: "#contact" },
+];
+</script>
+
+<template>
+  <nav>
+    <ul class="flex flex-col items-center">
+      <li v-for="item in navItems" :key="item.href">
+        <Button as-child variant="ghost" size="sm" class="w-40 px-0">
+          <a :href="item.href" :aria-label="`Navigate to ${item.text} section`">
+            {{ item.text }}
+          </a>
+        </Button>
+      </li>
+    </ul>
+  </nav>
+</template>
