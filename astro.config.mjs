7e78cb7f98810dd3d2fff5ad5c277e0b593438de
Ashton Heald <devtheash@gmail.com>
2025-05-13 18:43:03 -0500

    disabled astro dev tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
